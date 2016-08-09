45b6180ae7514fee28319b1429de613506adf621
Christian Nassif-Haynes <DoxasticFox@bitbucket.org>
2016-08-10 01:55:07 +1000

    Some tweaks to the grid interface and other stuff

---
+++ postproc.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+cd module
+
+string="Up=Up"
+replacement="Up=▲"
+perl -0777 -i.original -pe "s/\\Q$string/$replacement/igs" english.0.properties
+
+string="Down=Down"
+replacement="Down=▼"
+perl -0777 -i.original -pe "s/\\Q$string/$replacement/igs" english.0.properties
+
+string="Left=Left"
+replacement="Left=◀"
+perl -0777 -i.original -pe "s/\\Q$string/$replacement/igs" english.0.properties
+
+string="Right=Right"
+replacement="Right=▶"
+perl -0777 -i.original -pe "s/\\Q$string/$replacement/igs" english.0.properties
+
+rm english.0.properties.original
